a89ee99eeca310bfb3e709b55f0de5835b898403
fabius8 <fabius8@163.com>
2018-06-18 23:53:22 +0800

    --wip--

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-async function autoVote(interval = 60000) {
-
+async function autoVote(interval = 2000) {
     return setInterval(async () => {
-        const articles = await fetchFollowArticles();
-        for (let i = 0; i <  articles.length; ++ i) {
-            article = articles[i];
-            if (!article.up && article.ups < 50) {
-                console.table(article);
-                vote(article.id);
+        var now = new Date();
+        console.log(now.getMinutes(), now.getSeconds());
+        if (now.getMinutes() == 0 && now.getSeconds() < 5) {
+            const articles = await fetchFollowArticles();
+            for (let i = 0; i <  articles.length; ++ i) {
+                article = articles[i];
+                if (!article.up && article.ups < 100) {
+                    console.table(article);
+                    vote(article.id);
+                }
             }
         }
-    }, interval)
+    }, interval);
 }
 
 autoVote();
