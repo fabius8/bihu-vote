46a38ee903c263ea4b7c7130bb2ac43841451067
fabius8 <fabius8@163.com>
2018-06-18 23:55:26 +0800

    --wip--

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 async function autoVote(interval = 2000) {
     return setInterval(async () => {
         var now = new Date();
-        console.log(now.getMinutes(), now.getSeconds());
         if (now.getMinutes() == 0 && now.getSeconds() < 5) {
             const articles = await fetchFollowArticles();
             for (let i = 0; i <  articles.length; ++ i) {
